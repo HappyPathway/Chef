a5129a1d118dcc0823705737ee5598efcfcd807c
Dave Arnold <darnold@hashicorp.com>
2018-06-24 14:15:34 -0700

    adding nomad cookbook

--- cookbooks/nomad/recipes/default.rb
+++ cookbooks/nomad/recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe 'vault-cluster::default'
+
+remote_file "/tmp/nomad.zip" do
+    source node[:nomad][:download_url]
+end
+
+execute "unzip /tmp/nomad.zip" do
+    cwd "/usr/local/bin"
+end
+
+directory "/etc/nomad.d"
+
+cookbook_file "/etc/init/nomad.conf" do
+    source "nomad.conf"
+end
